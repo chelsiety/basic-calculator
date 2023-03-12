e935992db519fe95f6d1745140cdcd0015b09e11
Chelsie Ty <chelsiety@gmail.com>
2023-03-13 03:40:20 +0800

    Prevent screen overflow of the calculator when computed value is too big - Create roundOffComputedValue()     - Create 2 ways of displaying rounded values         - roundOffAccurately()         - convertToExponentialNotation()

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ equalButton.addEventListener('click', () => {
     else {
         previousDisplayValue.textContent = `${previousOperand} ${currentOperator} ${currentOperand} =`;
         operate();
+        roundOffComputedValue(currentOperand);
         currentDisplayValue.textContent = currentOperand;
         currentOperand = '';
     }
@@ -60,10 +61,12 @@ function convertPercentToDecimal() {
     console.log(currentOperator)
     console.log(currentOperand)
 */
-let previousOperand = '' 
-let currentOperand = '0'
+let previousOperand = ''; 
+let currentOperand = '0';
 let currentOperator = null;
-const maxDisplayCharacterLength = 12
+const maxDisplayCharacterLength = 12;
+const maxDecimalPlaces = 6;
+
 
 function appendNumber(number) {
     if (currentDisplayValue.textContent === '0') {
@@ -78,6 +81,7 @@ function appendOperator(operator) {
     if (currentOperand === '') return
     else if (previousOperand !== '') {
         operate();
+        roundOffComputedValue(currentOperand);
     }
     currentOperator = operator;
     previousOperand = currentOperand;
@@ -160,3 +164,85 @@ function clearCalculator() {
 
 
 
+
+/*
+TESTING
+
+const computedValue = 1285903715
+const maxDecimalPlaces = 6
+const maxDisplayCharacterLength = 12
+*/
+
+function roundOffAccurately(num, decimalPlaces) {
+    let roundedValue = parseFloat(Math.round (num +'e'+ decimalPlaces) +'e-'+ decimalPlaces);
+    currentOperand = roundedValue;
+    return currentOperand;
+}
+
+function convertToExponentialNotation(num, decimalPlaces) {
+    let roundedValue = num.toExponential(decimalPlaces)
+    currentOperand = roundedValue;
+    return currentOperand;
+}
+
+console.log(`computed value: ${currentOperand}`)
+console.log(`computed value string length: ${currentOperand.toString().length}`)
+
+
+
+// 
+function roundOffComputedValue(num) {
+    /* Function that rounds off the computed value based on whether the value is an integer or a decimal number
+        - Round off values to prevent screen overflow of the calculator when computed number is too big
+        - Two ways of displaying rounded values for values to display nicely in the calculator 
+            - roundOffAccurately function
+            - convertToExponentialNotation function
+        -  The maxDisplayCharacterLength, maxDecimalPlaces are chosen to fit according to the existing screen display of the calculator. Alter or change these values as needed
+        - Use the roundOffAccurately function... 
+            - when the integer is <= maxDisplayCharacterLength 
+            - when the decimal value is <= maxDecimalPlaces
+        - Use the convertToExponentialNotation function...
+            - when the integer is > maxDisplayCharacterLength 
+            - when the decimal value is > maxDecimalPlaces
+    */
+    
+     // Check if computed value is written in exponential notation 
+    if (num.toString().includes('e')) {
+        // Limit the decimal places of the number in exponential notation
+        return convertToExponentialNotation(num, maxDecimalPlaces)
+    }
+    
+    // Check if number is a float (decimal) or integer
+    
+    // Check if number is an integer 
+    if (Number.isInteger(num)) {
+        // Check if integer has less than or equal to 12 characters
+        if (num.toString().length <= maxDisplayCharacterLength) {
+            console.log('integer, <= 12, roundOffAccurately')
+            return roundOffAccurately(num, maxDecimalPlaces)
+        } else { 
+            console.log('integer, >12, convertToExponentialNotation')
+            return convertToExponentialNotation(num, maxDecimalPlaces)
+        }
+    }
+    // Check if  number is a float or decimal
+    if (!Number.isInteger(num)) {
+        
+        // Get the numbers before the decimal point
+        let splitNumArray = num.toString().split('.');
+        let beforeDecimalNum = splitNumArray[0]
+        
+        // Check if the number of digits before the decimal point is less than or equal to 6
+        if (beforeDecimalNum.length <= maxDecimalPlaces) {
+            console.log('decimal, <=6, roundOffAccurately')
+            return roundOffAccurately(num, maxDecimalPlaces)
+        }
+        else {
+           console.log('decimal, >6, convertToExponentialNotation')
+           return convertToExponentialNotation(num, maxDecimalPlaces)
+        }
+    }
+}
+console.log('----------------------')
+console.log('Result: ')
+console.log(roundOffComputedValue(currentOperand))
